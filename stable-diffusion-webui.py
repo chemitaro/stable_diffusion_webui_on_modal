96ad4490cdccb1b781cd8c8ac52d8a78e2702f57
chemitaro <iwasi_44@hotmail.com>
2023-05-17 12:03:04 +0900

    civitai.comからModel, LoRAをダウンロードし、使用するように変更

--- stable-diffusion-webui.py
+++ stable-diffusion-webui.py
@@ -15,16 +15,94 @@ volume_main = modal.SharedVolume().persist("stable-diffusion-webui-automatic1111
 # 色んなパスの定義
 webui_dir = "/content/stable-diffusion-webui"
 webui_model_dir = webui_dir + "/models/Stable-diffusion/"
+webui_lora_dir = webui_dir + "/models/Lora/"
+webui_texture_dir = webui_dir + "/embeddings/"
 
-# モデルのID
-model_ids = [
+# HuggingFaceのモデルのID
+# hugging_face_model_ids = [
+#     {
+#         "repo_id": "hakurei/waifu-diffusion-v1-4",
+#         "model_path": "wd-1-4-anime_e1.ckpt",
+#         "config_file_path": "wd-1-4-anime_e1.yaml",
+#     },
+# ]
+
+# Modelファイル
+model_files = [
     {
-        "repo_id": "hakurei/waifu-diffusion-v1-4",
-        "model_path": "wd-1-4-anime_e1.ckpt",
-        "config_file_path": "wd-1-4-anime_e1.yaml",
+        "url": "https://civitai.com/api/download/models/11745",
+        "file_name": "Chilloutmix-Ni-pruned-fp32-fix.safetensors",
+        "info": "https://civitai.com/models/6424/chilloutmix",
+    },
+    {
+        "url": "https://civitai.com/api/download/models/63786",
+        "file_name": "bra_v5.safetensors",
+        "info": "https://civitai.com/models/25494/brabeautiful-realistic-asians-v2",
+    },
+    {
+        "url": "https://civitai.com/api/download/models/29460",
+        "file_name": "realisticVisionV20_v20.safetensors",
+        "info": "https://civitai.com/models/4201/realistic-vision-v20",
+    },
+    {
+        "url": "https://civitai.com/api/download/models/15640",
+        "file_name": "uberRealisticPornMerge_urpmv13.safetensors",
+        "info": "https://civitai.com/models/2661/uber-realistic-porn-merge-urpm",
+    },
+    {
+        "url": "https://civitai.com/api/download/models/56401",
+        "file_name": "lazymixRealAmateur_v20.safetensors",
+        "info": "https://civitai.com/models/10961?modelVersionId=56401",
     },
 ]
 
+# Loraファイル
+lora_files = [
+    {
+        "url": "https://civitai.com/api/download/models/16677",
+        "file_name": "cuteGirlMix4_v10.safetensors",
+        "info": "https://civitai.com/models/14171/cutegirlmix4",
+    },
+    {
+        "url": "https://civitai.com/api/download/models/34562",
+        "file_name": "JapaneseDollLikeness_v15.safetensors",
+        "info": "https://civitai.com/models/28811/japanesedolllikeness-v15",
+    },
+    {
+        "url": "https://civitai.com/api/download/models/26413",
+        "file_name": "public_v1.0-000005.safetensors",
+        "info": "https://civitai.com/models/22123/in-public-photographers?modelVersionId=26413",
+    },
+    {
+        "url": "https://civitai.com/api/download/models/48983",
+        "file_name": "blacked_v1.5.safetensors",
+        "info": "https://civitai.com/models/44353/blacked",
+    },
+    {
+        "url": "https://civitai.com/api/download/models/10187",
+        "file_name": "skirtlift-v1.safetensors",
+        "info": "https://civitai.com/models/8631/skirtlift-the-astonishing-sequel-to-shirtlift",
+    },
+    {
+        "url": "https://civitai.com/api/download/models/19492",
+        "file_name": "GodPussy2_Innie.safetensors",
+        "info": "https://civitai.com/models/10332/realistic-vaginas-god-pussy-2-innie",
+    },
+    {
+        "url": "https://civitai.com/api/download/models/18077",
+        "file_name": "Creampie_v11.safetensors",
+        "info": "https://civitai.com/models/14557/creampie-and-hairy-pussy",
+    },
+]
+
+# Textureファイル
+texture_files = [
+    # {
+    #     "url": "",
+    #     "file_name": "",
+    #     "info": "",
+    # }
+]
 
 @stub.function(
     image=modal.Image.from_dockerhub("python:3.10")
@@ -89,46 +167,57 @@ model_ids = [
     timeout=6000,
 )
 async def run_stable_diffusion_webui():
-    print(Fore.CYAN + "\n---------- セットアップ開始 ----------\n")
+    print(Fore.CYAN + "\n---------- Setup Start ----------\n")
 
     webui_dir_path = Path(webui_model_dir)
     if not webui_dir_path.exists():
         subprocess.run(f"git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui {webui_dir}", shell=True)
 
+    # Modelファイルをダウンロードし、保存する
+    for model_file in model_files:
+        download_file(model_file["url"], webui_model_dir + model_file["file_name"])
+
+    # Loraファイルをダウンロードし、保存する
+    for lora_file in lora_files:
+        download_file(lora_file["url"], webui_lora_dir + lora_file["file_name"])
+
+    # Textureファイルをダウンロードし、保存する
+    for texture_file in texture_files:
+        download_file(texture_file["url"], webui_texture_dir + texture_file["file_name"])
+
     # Hugging faceからファイルをダウンロードしてくる関数
-    def download_hf_file(repo_id, filename):
-        from huggingface_hub import hf_hub_download
+    # def download_hf_file(repo_id, filename):
+    #     from huggingface_hub import hf_hub_download
 
-        download_dir = hf_hub_download(repo_id=repo_id, filename=filename)
-        return download_dir
-
-
-    for model_id in model_ids:
-        print(Fore.GREEN + model_id["repo_id"] + "のセットアップを開始します...")
-
-        if not Path(webui_model_dir + model_id["model_path"]).exists():
-            # モデルのダウンロード＆コピー
-            model_downloaded_dir = download_hf_file(
-                model_id["repo_id"],
-                model_id["model_path"],
-            )
-            shutil.copy(model_downloaded_dir, webui_model_dir + os.path.basename(model_id["model_path"]))
+    #     download_dir = hf_hub_download(repo_id=repo_id, filename=filename)
+    #     return download_dir
 
 
-        if "config_file_path" not in model_id:
-            continue
+    # for model_id in hugging_face_model_ids:
+    #     print(Fore.GREEN + model_id["repo_id"] + "のセットアップを開始します...")
 
-        if not Path(webui_model_dir + model_id["config_file_path"]).exists():
-            # コンフィグのダウンロード＆コピー
-            config_downloaded_dir = download_hf_file(
-                model_id["repo_id"], model_id["config_file_path"]
-            )
-            shutil.copy(config_downloaded_dir, webui_model_dir + os.path.basename(model_id["config_file_path"]))
+    #     if not Path(webui_model_dir + model_id["model_path"]).exists():
+    #         # モデルのダウンロード＆コピー
+    #         model_downloaded_dir = download_hf_file(
+    #             model_id["repo_id"],
+    #             model_id["model_path"],
+    #         )
+    #         shutil.copy(model_downloaded_dir, webui_model_dir + os.path.basename(model_id["model_path"]))
 
 
-        print(Fore.GREEN + model_id["repo_id"] + "のセットアップが完了しました！")
+    #     if "config_file_path" not in model_id:
+    #         continue
 
-    print(Fore.CYAN + "\n---------- セットアップ完了 ----------\n")
+    #     if not Path(webui_model_dir + model_id["config_file_path"]).exists():
+    #         # コンフィグのダウンロード＆コピー
+    #         config_downloaded_dir = download_hf_file(
+    #             model_id["repo_id"], model_id["config_file_path"]
+    #         )
+    #         shutil.copy(config_downloaded_dir, webui_model_dir + os.path.basename(model_id["config_file_path"]))
+
+        # print(Fore.GREEN + model_id["repo_id"] + "のセットアップが完了しました！")
+
+    print(Fore.CYAN + "\n---------- Setup Complete ----------\n")
 
     # WebUIを起動
     sys.path.append(webui_dir)
@@ -138,9 +227,25 @@ async def run_stable_diffusion_webui():
 
     prepare_environment()
     # 最初のargumentは無視されるので注意
-    sys.argv = shlex.split("--a --gradio-debug --share --xformers")
+    sys.argv = shlex.split("--a --gradio-debug --share --xformers --enable-insecure-extension-access")
     start()
 
+# 指定のURLからファイルをダウンロードし、指定したパスに保存する
+@stub.function()
+def download_file(url, path):
+    # ファイルのダウンロード
+    subprocess.run(
+        shlex.split(f"wget {url} -O {path}"),
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+
+    # ファイルの存在確認
+    if not os.path.exists(path):
+        print(Fore.RED + f"File not found: {path}")
+        sys.exit(1)
+
+    print(Fore.GREEN + f"Downloaded: {path}")
 
 @stub.local_entrypoint()
 def main():
